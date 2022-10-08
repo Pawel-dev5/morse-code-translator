fc4428b12d1b9f089971312e4464de36ab6ad00e
Pawel-dev5 <p.nowecki@gmail.com>
2022-10-08 21:56:41 +0200

    [R] refactor structure and add context

---
+++ src/components/sections/Buttons.tsx
@@ -0,0 +1,38 @@
+import { useContext } from 'react';
+import GitHubButton from 'react-github-btn';
+
+// CONTEXT
+import { ContextData } from 'components/hooks/useMorseTranslator';
+import { StyledButton } from 'components/sections/Styles';
+
+export const Buttons = () => {
+	const { onButtonMouseDown, onButtonMouseUp, resetAll } = useContext(ContextData);
+
+	const buttonTitle = `Kliknij aby napisać .
+Przytrzymaj aby napisać -`;
+
+	return (
+		<>
+			<GitHubButton href="https://github.com/Pawel-dev5/morse-code-translator">Github</GitHubButton>
+
+			<StyledButton
+				type="button"
+				autoFocus
+				title={buttonTitle}
+				aria-label={buttonTitle}
+				onMouseDown={onButtonMouseDown}
+				onMouseUp={onButtonMouseUp}
+				onKeyDown={(e) => e.code === 'Space' && onButtonMouseDown(e)}
+				onKeyUp={(e) => e.code === 'Space' && onButtonMouseUp(e)}
+				tabIndex={0}
+				customPadding="3rem"
+			>
+				Kliknij lub przytrzymaj
+			</StyledButton>
+
+			<StyledButton type="button" title="Reset" aria-label="Reset" onClick={() => resetAll()}>
+				Reset
+			</StyledButton>
+		</>
+	);
+};
---
+++ src/components/sections/Styles.ts
@@ -0,0 +1,31 @@
+import styled, { css } from 'styled-components';
+
+export const StyledInstrunctionWrapper = styled.div`
+	display: flex;
+	flex-direction: column;
+	align-items: start;
+	justify-content: center;
+	width: 100%;
+	padding: 1rem;
+`;
+
+export const StyledAlphabetWrapper = styled.div`
+	display: grid;
+	grid-template-columns: 1fr 2fr;
+	grid-gap: 0.5rem;
+	padding: 0.5rem 0;
+	text-align: left;
+`;
+
+export const StyledButton = styled.button<{ customPadding?: string }>`
+	padding: 0.5rem 1rem;
+	border: none;
+	border-radius: 0.25rem;
+	user-select: none;
+
+	${({ customPadding }) =>
+		customPadding &&
+		css`
+			margin-top: ${customPadding};
+		`}
+`;
